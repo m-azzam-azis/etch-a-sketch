7cc4e96e80fcf0e7311585645f5e3e2fa00a4edc
m.azzam <m.azzam.azis@gmail.com>
2024-02-16 16:49:07 +0700

    Feature update
    added random color and darken them when hovered multiple times
    also clean up some html, js, css syntax and styling

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ function main() {
   createGrid(16);
   const button = document.querySelector('button');
   button.addEventListener('click', () => {
-    do {
-      gridSize = prompt('enter desired grid size (1-100)')
-      if (gridSize < 1 || gridSize > 100) {
-        alert('please enter an integer 1-100');
-      }
-    } while (gridSize < 1 || gridSize > 100);
 
-    removeGrid();
-    createGrid(gridSize);
+    gridSize = prompt('enter desired grid size (1-100)')
+    if (gridSize < 1 || gridSize > 100) {
+      alert('please enter an integer 1-100');
+    } else {
+      removeGrid();
+      createGrid(gridSize);
+    }
+    
   });
 
 }
@@ -25,22 +25,43 @@ function createGrid (gridSize) {
 
     const gridRow = document.createElement('div');
     gridRow.style.height = `${containerSize/gridSize}px`;
-    gridRow.className = 'grid-row';
+    gridRow.classList.add('grid-row');
 
     for (let j = 0; j < gridSize; j++) {
 
       const gridBox = document.createElement('div');
       gridBox.style.width = `${containerSize/gridSize}px`;
-      gridBox.className = 'grid-box';
-      // gridBox.textContent = i*gridSize +j;
+      gridBox.classList.add('grid-box');
+
       gridRow.appendChild(gridBox);
 
       gridBox.addEventListener('mouseover', () => {
-        gridBox.style.backgroundColor = 'black';
+        if (!gridBox.classList.contains('colored')) {
+          hue = generateColor()[0];
+          saturation = generateColor()[1];
+          lightness = generateColor()[2];
+          gridBox.style.backgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+          gridBox.classList.add('colored');
+
+        } else if (lightness-10 > 0) {
+          lightness -= 10;
+          gridBox.style.backgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+
+        } else {
+          gridBox.style.backgroundColor = `hsl(${hue}, ${saturation}%, 0%)`;
+        }
       });
     }
     container.append(gridRow);
   }
+}
+
+function generateColor() {
+  let hue = Math.floor(Math.random() * 361);
+  let saturation = Math.floor(Math.random() * 101);
+  let lightness = 100 - Math.floor(Math.random() * 11);
+
+  return [hue, saturation, lightness]
 }
 
 function removeGrid() {
